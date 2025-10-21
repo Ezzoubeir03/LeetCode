12abe66d00c8c452b513cbb181acaeedeff53f2f
Ezzoubeir <ezzoubeirelhalami@gmail.com>
2025-10-21 19:50:17 +0800

    first commit

---
+++ Length_of_Last_Word.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    s = s.trim();
+    let words = s.split(' ');
+    return words[words.length - 1].length;
+    //here in this Problem i am gonna need to calculate the length of the last word in the sentence..
+    let subs = s.substring();
+    
+};
