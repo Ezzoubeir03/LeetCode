ecc35c6ef49a05981290c7ee1fd21cb855acebab
Ezzoubeir03 <ezzoubeirelhalami@gmail.com>
2025-10-23 23:42:39 +0800

    Time: 0 ms (100.00%) | Memory: 56.4 MB (90.56%) - LeetSync

---
+++ 112-path-sum/path-sum.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    //our target is to return the sum of the nodes from the top to the bottum 
+    //to achieve our target:
+    
+   if(!root) return false;
+   if(!root.left && !root.right){
+     return root.val === targetSum;
+   }
+  
+    return hasPathSum(root.left,targetSum - root.val) || hasPathSum(root.right,targetSum - root.val);
+};
