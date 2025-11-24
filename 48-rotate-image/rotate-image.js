adcbf92ce5f4899b6d39a75a06388cdf9fea6847
Ezzoubeir03 <ezzoubeirelhalami@gmail.com>
2025-11-24 20:47:37 +0800

    Time: 0 ms (100.00%) | Memory: 55.9 MB (28.34%) - LeetSync

---
+++ 48-rotate-image/rotate-image.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    ///Rotate Image:
+    /// first thing is to rotate the rows to be a columns this our goal :
+    for(let i = 0; i < matrix.length; i++){
+    for(let j = i+1; j < matrix.length; j++){
+     [matrix[i][j],matrix[j][i]] = [matrix[j][i],matrix[i][j]];
+       }
+    }
+    for(let i = 0; i < matrix.length;i++){
+       matrix[i].reverse();
+   
+    }
+
+    //return matrix;
+};
