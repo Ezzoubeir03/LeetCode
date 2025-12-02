97325e85e39ed7c1110c84a8936091e4f1e6a4c2
Ezzoubeir03 <ezzoubeirelhalami@gmail.com>
2025-12-02 14:50:14 +0800

    Time: 40 ms (47.61%) | Memory: 80.4 MB (45.53%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    //Longest Consecutive Sequence :
+    //Algorithm that runs in O(n) time :
+    let set = new Set(nums);
+    let longest = 0;
+    for (let num of set){
+    if(!set.has(num -1)){
+        let current = num;
+        let count = 1;
+        while(set.has(current + 1)){
+            current++;
+            count++;
+        }
+        longest = Math.max(longest, count);
+    }
+    } 
+    return longest;
+};
