62c0d56932b6c02069ffaf1a2a90d8ce7ee3dfe9
Ezzoubeir03 <ezzoubeirelhalami@gmail.com>
2026-01-10 21:41:58 +0800

    Time: 0 ms (100.00%) | Memory: 56.5 MB (68.46%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+/// Minimum Path Sum 
+///we have two options either to movie down or right at any pointin time .
+const m = grid.length;
+const n = grid[0].length;
+for(let i = 0; i < m; i++){
+    for(let j = 0; j < n; j++){
+        if(i === 0 && j === 0) continue;
+    else if(i === 0) grid[i][j] += grid[i][j -1];
+    else if(j === 0) grid[i][j] += grid[i-1][j];
+    else grid[i][j] += Math.min(grid[i -1][j], grid[i][j -1]);
+
+    }
+}
+return grid[m -1][n -1];
+};
