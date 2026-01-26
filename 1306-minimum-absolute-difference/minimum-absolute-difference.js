51dc3dc8c42294ba39985a9df74bc8679438a219
Ezzoubeir03 <ezzoubeirelhalami@gmail.com>
2026-01-26 23:39:28 +0800

    Time: 51 ms (90.66%) | Memory: 70.4 MB (39.45%) - LeetSync

---
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+var minimumAbsDifference = function(arr) {
+    arr.sort((a, b) => a - b );
+    const result = [];
+    let min = Infinity;
+    for(let i = 0; i < arr.length -1; i++){
+    let dif = arr[i + 1] - arr[i];
+    min = Math.min(dif, min);
+    }
+   for(let i = 0; i < arr.length - 1; i++){
+    let dif = arr[i + 1] - arr[i];
+    if(min === dif){
+        result.push([arr[i], arr[i + 1]]);
+    }
+   }
+   return result;
+};
