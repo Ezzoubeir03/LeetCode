0e7eee32d97dfbdc3128307a8ab3910ba02fe439
Ezzoubeir03 <ezzoubeirelhalami@gmail.com>
2026-02-18 22:06:01 +0800

    Time: 0 ms (100.00%) | Memory: 52.4 MB (100.00%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.js
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.js
@@ -6,11 +6,10 @@ var hasAlternatingBits = function(n) {
     ////Binary Number with Alternating Bits:
     ////n.toString(2);///converting from decimal to binary :
     let result = n.toString(2);
-
-    for(let i = 1; i <= result.length; i++){
-        if(result[i] === result[i-1]){
+    for(let i = 0; i <= result.length; i++){
+        if(result[i] === result[i - 1]){
             return false;
         }
     }
-           return true;
+    return true;
 };
