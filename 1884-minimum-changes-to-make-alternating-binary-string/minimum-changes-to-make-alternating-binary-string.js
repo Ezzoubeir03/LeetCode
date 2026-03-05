f7e499f199d39d3dc9fa9c784f3f567bd5730d35
Ezzoubeir03 <ezzoubeirelhalami@gmail.com>
2026-03-05 23:54:23 +0800

    Time: 1 ms (89.13%) | Memory: 54.1 MB (63.04%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minOperations = function(s) {
+    ///Minimum Changes To Make Alternating Binary String :
+
+    ///if it is already alternating we need to return 0 otherwise return 1;
+
+   let count1 = 0;
+   let count2 = 0;
+
+    ///let Binary = s.toString(2);
+
+    for(let i = 0; i < s.length; i++){
+         if(s[i] === (i % 2 === 0 ?'0' : '1')){
+           count1++;
+         }
+         if(s[i] === (i % 2 === 0 ?'1' : '0')){
+            count2++;
+         }
+
+         //if(s[i] !== expected1) count1++;
+         //if(s[i] !== expected2) count2++;
+    }
+    return Math.min(count1, count2);
+};
