3f16553742832d60ec4b9530a25bfea6a66cb03c
Ezzoubeir03 <ezzoubeirelhalami@gmail.com>
2026-03-07 01:03:22 +0800

    Time: 1 ms (23.81%) | Memory: 54.6 MB (11.90%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.js
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.js
@@ -3,12 +3,10 @@
  * @return {boolean}
  */
 var checkOnesSegment = function(s) {
-////Check if Binary String Has at Most One Segment Ones:
-////returning true if s contains at most one contiguous segment of ones, otherwise return false:
-for(let i = 0; i <=s.length - 1; i++){
-    if(s[i] ==='0' && s[i+ 1] === '1'){
+  for(let i = 0; i <= s.length - 1; i++){
+     if(s[i] ==='0' && s[i+ 1] === '1'){
         return false;
-       }
+        }
    }
  return true;
 };
