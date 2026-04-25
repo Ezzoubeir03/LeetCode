4364fdc50316b5da3615258c871eadb7f62055cd
Ezzoubeir03 <ezzoubeirelhalami@gmail.com>
2026-04-25 22:28:58 +0800

    Time: 0 ms (100.00%) | Memory: 54 MB (37.61%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+   let max = 0;
+   for(let i = 0; i< nums.length; i++){
+    if(nums[i] > nums[max]){
+        max = i;
+    }
+   }
+   return max;
+};
